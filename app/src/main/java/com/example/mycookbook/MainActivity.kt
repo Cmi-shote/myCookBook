3017e8a448f4a44f6827b0722a051687cd605eae
similoluwashote <simi.shote@babbangona.com>
2025-05-29 16:14:03 +0100

    #feat: fixed navigation issue

--- app/src/main/java/com/example/mycookbook/MainActivity.kt
+++ app/src/main/java/com/example/mycookbook/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.example.mycookbook.presentation.navigation.AppNavigation
 import com.example.mycookbook.presentation.navigation.AppRoute
 import com.example.mycookbook.presentation.utils.UserPreferences
 import com.example.mycookbook.ui.theme.MyCookBookTheme
+import kotlinx.coroutines.flow.first
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,12 +26,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyCookBookTheme {
                 val navController = rememberNavController()
-                val onboardingSeen by userPreferences.onboardingSeen.collectAsState(initial = false)
+                var nextDestination by remember { mutableStateOf<AppRoute?>(null) }
 
-                AppNavigation(
-                    navController = navController,
+                LaunchedEffect(Unit) {
+                    val onboardingSeen = userPreferences.onboardingSeen.first()
                     nextDestination = if (onboardingSeen) AppRoute.MainWithBottomNav else AppRoute.OnboardingRoute
-                )
+                }
+
+                nextDestination?.let { destination ->
+                    AppNavigation(
+                        navController = navController,
+                        nextDestination = destination
+                    )
+                }
             }
         }
     }
